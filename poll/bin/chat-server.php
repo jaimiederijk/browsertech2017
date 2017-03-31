661c7ef989189b76495ae08d451e5e498335effa
jaimiederijk <j.r.derijk@quicknet.nl>
2017-03-31 07:59:58 +0200

    basis

---
+++ poll/bin/chat-server.php
@@ -0,0 +1,15 @@
+<?php
+
+    require __DIR__ . '/vendor/autoload.php';
+
+use Ratchet\Server\IoServer;
+use MyApp\Chat;
+
+    // require dirname(__DIR__) . '/vendor/autoload.php';
+
+    $server = IoServer::factory(
+        new Chat(),
+        8080
+    );
+
+    $server->run();
---
+++ poll/index.php
@@ -0,0 +1,63 @@
+<?php
+  // include('simple_html_dom.php');
+  // $html = new simple_html_dom();
+
+  // $html->load_file('http://www.knzb.nl/vereniging__wedstrijdsport/wedstrijdsport/waterpolo/uitslagen_waterpolo/verenigingen/poule/3862/1376/DHR906C/');
+
+  // $elementH1wedstrijd = $html->find('.aankomende-wedstrijd-blok', 0);
+
+
+  //   unset($html);
+
+  // $html = new simple_html_dom();
+  // $html->load_file('http://www.knzb.nl/vereniging__wedstrijdsport/wedstrijdsport/waterpolo/uitslagen_waterpolo/verenigingen/poule/3864/1377/DHR906C/');
+
+  // $elementH2wedstrijd = $html->find('.aankomende-wedstrijd-blok', 0);
+
+  // unset($html);
+
+  // $html = new simple_html_dom();
+  // $html->load_file('http://www.knzb.nl/vereniging__wedstrijdsport/wedstrijdsport/waterpolo/uitslagen_waterpolo/verenigingen/poule/3872/4363/DHR906C/');
+
+  // $elementJeugdwedstrijd = $html->find('.aankomende-wedstrijd-blok', 0);
+
+
+?>
+<html>  
+  <head>      
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/app.css">
+    <title>Waterpolo</title>
+    <script>
+      'article aside footer header nav section time'.replace(/\w+/g,function(n){document.createElement(n)});
+    </script>
+
+  </head>
+  <body>    
+    <div id="wrapper">
+      
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="createpoll.php">Create poll</a>
+            </li>
+          </ul>
+        </nav>
+
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  </body>
+  
+  <script src="javascript/app.js"></script>
+</html>  
